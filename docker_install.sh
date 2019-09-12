5e2d126c8513417e26844ad008f07522e46c9b38
magic233 <mywh617171928@live.com>
2019-09-12 17:15:14 +0800

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo yum remove docker \
+yum remove docker \
 docker-client \
 docker-client-latest \
 docker-common \
@@ -9,11 +9,11 @@ docker-logrotate \
 docker-selinux \
 docker-engine-selinux \
 docker-engine
-echo yum install -y yum-utils device-mapper-persistent-data lvm2 
-echo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-echo yum makecache fast
-echo yum -y install docker-ce
-echo systemctl start docker
+yum install -y yum-utils device-mapper-persistent-data lvm2 
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache fast
+yum -y install docker-ce
+systemctl start docker
 clear
 echo "======================"
 echo "=Install successfully="
